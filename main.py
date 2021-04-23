fad0cfe1c4d4adc1bb889ad5ff1be0c96ea5c847
Pawarit Thareechan <root@lckyn.com>
2021-04-23 23:04:44 +0700

    add line bg

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
-# Import and initialize the pygame library
 import pygame
 import config
 
 
 pygame.init()
 
-# Set up the drawing window
 screen = pygame.display.set_mode([config.size['x'], config.size['y']])
 
-# Run until the user asks to quit
 running = True
 while running:
-
-    # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Fill the background with white
     screen.fill((255, 255, 255))
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    # draw background
+    for i in range(1,4):
+        pygame.draw.line(screen, (0, 0, 0), (i*125,0), (i*125,1000), 1)
 
-    # Flip the display
     pygame.display.flip()
-
-# Done! Time to quit.
 pygame.quit()
