759ec55e990107cc16b3a7ed0eb4a1c8de9b1d83
Pawarit Thareechan <root@lckyn.com>
2021-04-23 23:17:53 +0700

    update config file

--- config.py
+++ config.py
@@ -1,4 +1,16 @@
 size = dict(
-    x = 500,
-    y = 1000
+    x=500,
+    y=1000
+)
+
+scale = dict(
+    v_line=size["x"] / 4,
+    v_line_size=1,
+    h_line=size["y"] * 0.95,
+    h_line_size=5
+)
+
+color = dict(
+    line=(0, 0, 0),
+    box=(40, 40, 40)
 )
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 import config
 
+size = config.size
+scale = config.scale
+color = config.color
 
 pygame.init()
 
-screen = pygame.display.set_mode([config.size['x'], config.size['y']])
+screen = pygame.display.set_mode([size["x"], size["y"]])
 
 running = True
 while running:
@@ -15,8 +18,13 @@ while running:
     screen.fill((255, 255, 255))
 
     # draw background
-    for i in range(1,4):
-        pygame.draw.line(screen, (0, 0, 0), (i*125,0), (i*125,1000), 1)
+    for i in range(1, 4):
+        pygame.draw.line(screen, color["line"],
+                         (i * scale["v_line"], 0), (i * scale["v_line"], size["y"]), scale["v_line_size"])
+    pygame.draw.line(screen, color["line"],
+                     (0, scale["h_line"]), (size["x"], scale["h_line"]), scale["h_line_size"])
+
+    pygame.draw.rect(screen, (40, 40, 40), pygame.Rect(10, 10, 105, 30))
 
     pygame.display.flip()
 pygame.quit()
